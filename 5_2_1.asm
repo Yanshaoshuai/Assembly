79ce0d3ae556f2eda3450bf0edb8de92e7df9b7b
yanshaoshuai <ahutyss@gmail.com>
2022-05-11 23:53:45 +0800

    first commit

---
+++ 5_2_1.asm
@@ -0,0 +1,8 @@
+assume cs:code
+code segment
+    mov ax,2
+    mov ax,ax
+    mov ax,4c00H
+    int 21H
+code ends
+end
---
+++ t1.asm
@@ -0,0 +1,19 @@
+// 初始值 21000H=be 21001H=00
+assume cs:codesg
+codesg segment
+
+    mov ax,2000H
+    mov ss,ax
+    mov sp,0
+    add sp,10
+    pop ax
+    pop bx
+    push ax
+    push bx
+
+    mov ax,4c00H
+    int 21H
+
+codesg ends
+end
+    
